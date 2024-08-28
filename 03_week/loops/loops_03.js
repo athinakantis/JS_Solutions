96f59b44415be2d95380f4f95db92e90bab802a8
[Athina Kantis] <[athina.kantis@gmail.com]>
2024-08-28 12:36:56 +0300

    Add loop solutions

---
+++ 03_week/loops/loops_03.js
@@ -0,0 +1,8 @@
+// Create a program that continuously prompts the user to input distance (in kilometers) and time (in hours) and then calculates the average speed. The program should terminate when the user enters 0 for the distance. Upon receiving a distance of 0, the program should not prompt for any further input.
+
+let km = prompt('How many kilometers?')
+let h = prompt('How many hours?')
+
+function averageSpeed(km, h) {
+    console.log(`Average speed is ${km/h}`)
+}
---
+++ 03_week/loops/loops_04.js
@@ -0,0 +1,10 @@
+// Develop a program that requests the user to input 20 numbers. After all numbers are entered, the program should display how many of these numbers are even. Do not use array.
+
+function howManyEven() {
+    let count = 0
+    for (let i = 0; i <= 20; i++){
+        let num = prompt('Please write a number')
+        if (num % 2 === 0) {count++}
+    }
+    console.log(`You wrote ${count} equal numbers`)
+}
