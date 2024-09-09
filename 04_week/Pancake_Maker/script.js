8d9e2d839765ff144d3b985e59bca22b67a1b78e
[Athina Kantis] <[athina.kantis@gmail.com]>
2024-09-09 15:26:22 +0300

    add order summary

--- 04_week/Pancake_Maker/script.js
+++ 04_week/Pancake_Maker/script.js
@@ -1,45 +1,74 @@
 
-let pancakeSelect = document.querySelector('#type')
-let toppings = document.querySelectorAll('#nuts, #bananas, #syrup')
-let extras = document.querySelectorAll('#whippedCream, #iceCream')
 let displayPrice = document.querySelectorAll('#totalPrice')
-let totalPrice = +pancakeSelect.value
+let basePrice = +document.querySelector('#type').value
+let toppingsPrice = 0;
+let deliveryCost = 0;
+let pancake = {pancakeBase: 'Classic', toppings: [], extras: []}
 
 
-
-pancakeSelect.addEventListener('change', () => {
-    totalPrice = +pancakeSelect.value
+document.addEventListener('change', (e) => {
+    if (e.target.id === 'type') {
+        basePrice = +e.target.value
+        pancake.pancakeBase = e.target.selectedOptions[0].textContent.match(/[a-z]/gi).join('')
+    } else if (e.target.name === 'topping') {
+        if (e.target.checked) {
+            toppingsPrice ++
+            pancake.extras.push(e.target.id)
+        } else {
+            toppingsPrice--
+            pancake.extras.filter((a) => a !== e.target.id)
+        }
+    } else if (e.target.name === 'extra') {
+        if (e.target.checked) {
+            toppingsPrice++
+            pancake.extras.push(e.target.id)
+        } else {
+            toppingsPrice--
+            pancake.extras.filter((a) => a !== e.target.id)
+        }
+        e.target.checked ? toppingsPrice += +e.target.value : toppingsPrice -= +e.target.value
+    } else if (e.target.name === 'delivery') {
+        e.target.id === 'delivery' ? deliveryCost = 5 : deliveryCost = 0
+    }
     updatePrice()
 })
 
 
 
-for (let topping of toppings) {
-    topping.addEventListener('change', () => {
-        topping.checked ? totalPrice++ : totalPrice--
-        updatePrice()
-    })
-}
-
-for (let extra of extras) {
-    extra.addEventListener('change', () => {
-        if (extra.checked) {
-            totalPrice += +extra.value
-            updatePrice()
-        } else {
-            totalPrice -= +extra.value
-            updatePrice()
-        }
-    })
-}
-
-
 
 function updatePrice() {
     for (let element of displayPrice) {
-        element.textContent = `$${totalPrice}`
+        element.textContent = `$${basePrice + toppingsPrice + deliveryCost}`
         element.classList.add('bounce')
         setTimeout(() => element.classList.remove('bounce'), 500)
     }
 }
 
+let button = document.querySelector('button')
+let orderDetails = document.querySelectorAll('.summary p')
+
+
+
+
+button.addEventListener('click', () => {
+
+    //Displaying customer name
+    orderDetails[0].textContent = `Name: ${document.querySelector('#customerName').value}`
+
+    
+
+    for (let extra of pancake.extras) {
+        extra = extra.split('C')
+        extra[0][0] = extra[0][0].toUpperCase()
+        extra = extra.join(' C')
+        console.log(extra)
+    }
+    //Displaying pancake details
+    orderDetails[1].textContent = `Your order: ${pancake.pancakeBase} pancake, Toppings: ${pancake.toppings}, Extras: ${pancake.extras}`
+ 
+
+    //Displaying delivery cost
+    if (deliveryCost === 5) {
+        orderDetails[3].textContent = `Delivery fee: `
+    }
+})
