f998b2a3832bf7e60e4225f01339ce2a84465859
[Athina Kantis] <[athina.kantis@gmail.com]>
2024-09-22 17:40:44 +0300

    add cart feature

--- 04_week/Pancake_Maker/script.js
+++ 04_week/Pancake_Maker/script.js
@@ -1,10 +1,13 @@
 
-let displayPrice = document.querySelectorAll('#totalPrice')
+const displayPrice = document.querySelectorAll('#totalPrice')
 let basePrice = +document.querySelector('#type').value
 let toppingsPrice = 0;
 let deliveryCost = 0;
+let totalPancakeCount = 0;
 let pancake = {pancakeBase: 'Classic', toppings: [], extras: []}
-const savedOrders = []
+const allOrders = []
+let ticket = []
+
 const reply = document.querySelector('.reply')
 
 
@@ -34,7 +37,7 @@ document.addEventListener('change', (e) => {
         }
     } else if (e.target.name === 'delivery') {
         e.target.id === 'delivery' ? deliveryCost = 5 : deliveryCost = 0
-    } 
+    }
     updatePrice()
 })
 
@@ -44,72 +47,143 @@ document.addEventListener('change', (e) => {
 /* Update Price */
 function updatePrice() {
     for (let element of displayPrice) {
-        element.textContent = `$${basePrice + toppingsPrice + deliveryCost}`
+        element.textContent = `$${basePrice + toppingsPrice}`
         element.classList.add('bounce')
         setTimeout(() => element.classList.remove('bounce'), 500)
     }
 }
 
-
+function updateCart() {
+    console.log(ticket)
+    document.querySelector('.orderCount').classList.remove('hidden')
+    count.textContent = totalPancakeCount
+    resetPancake()
+}
 
 
 
 //Show order
-const seeOrderBtn = document.querySelector('#seeOrder')
+const viewOrderBtn = document.querySelector('#viewOrder')
 const summaryDetails = document.querySelectorAll('.summary p')
+const orderDisplay = document.querySelector('.orderDisplay')
 
-seeOrderBtn.addEventListener('click', () => {
-    let customerName = document.querySelector('#customerName').value
-    document.querySelector('.summary').classList.toggle('hidden')
-    document.querySelector('.customize').classList.toggle('hidden')
-    document.querySelector('#nameDisplay').textContent = `Name: ${customerName}`
+viewOrderBtn.addEventListener('click', () => {
+    toggleSummary()
+    displayOrder()
+})
 
-    
-    let formattedExtras = []
-    for (let extra of pancake.extras) {
-        extra += ' Cream'
-        formattedExtras.push(extra)
+
+
+
+//Add to cart event listener and functionality
+const count = document.querySelector('#orderCount')
+document.querySelector('#addToCart').addEventListener('click', () => {
+    totalPancakeCount++
+
+    if (ticket.length === 0) {
+        ticket.push({count: 1, pancake: pancake, cost: basePrice + toppingsPrice})
+        resetPancake()
+        return updateCart()
     }
 
+    const pancakeIndex = ticket.findIndex((item) => {
+        return JSON.stringify(item.pancake) === JSON.stringify(pancake)
+    })
 
-    document.querySelector('#orderDisplay').textContent = `${pancake.pancakeBase} pancake, Toppings: ${pancake.toppings.length > 0 ? pancake.toppings.join(', ') : 'None'}, Extras: ${pancake.extras.length > 0 ? formattedExtras.join(', ') : 'None'}`
- 
-
-    if (deliveryCost === 5) {
-        document.querySelector('#deliveryCost').textContent = `, including a $5 delivery fee`
+    if (pancakeIndex !== -1) {
+        ticket[pancakeIndex].count++
+        ticket[pancakeIndex].cost += basePrice + toppingsPrice
+        resetPancake()
+        return updateCart()
+    } else {
+        ticket.push({count: 1, pancake: pancake, cost: basePrice + toppingsPrice})
+        resetPancake()
+        return updateCart()
     }
 })
+
+function clearOrderDisplay() {
+    orderDisplay.innerHTML = ''
+}
+
+
+function displayOrder() {
+    if (ticket.length < 1) {
+        orderDisplay.textContent = `Your cart is empty, try adding some delicious pancakes!`
+    } else {
+        ticket.forEach((item) => {  
+            let html = `<div>
+                        <p>${item.count}x ${item.pancake.pancakeBase} pancake, Toppings: ${item.pancake.toppings.length > 0 ? item.pancake.toppings.join(', ') : 'None'} Extras: ${item.pancake.extras.length > 0 ? item.pancake.extras.join(', ') : 'None'} Cost: $${item.cost}</p>
+                        <div>
+                        <button class="orderAdd">+</button>
+                        <button class="orderRemove">-</button>
+                        </div>
+                        </div>`
+            orderDisplay.innerHTML += html
+            listenAddRemove()
+        })
+    }
+}
+
+
+/* Add more/Remove from cart functionality */
+function listenAddRemove() {
+    document.querySelectorAll('.orderAdd').forEach((button, index) => {
+        button.addEventListener('click', () => {
+            ticket[index].cost += ticket[index].cost / ticket[index].count
+            ticket[index].count++
+            totalPancakeCount++
+            updateCart()
+            clearOrderDisplay()
+            displayOrder()
+        })
+    })
+
+    document.querySelectorAll('.orderRemove').forEach((button, index) => {
+        button.addEventListener('click', () => {
+            if (ticket[index].count > 1) {
+                ticket[index].cost -= ticket[index].cost / ticket[index].count
+                ticket[index].count--
+            } else {
+                ticket = ticket.filter((a) => a !== a[index])
+            }
+            totalPancakeCount--
+            updateCart()
+            clearOrderDisplay()
+            displayOrder()
+        })
+    })
+}
+
+
 
 
 //Return to pancake customization
 const returnBtn = document.querySelector('#return')
-returnBtn.addEventListener('click', () => toggleSummary())
+returnBtn.addEventListener('click', () => {
+    toggleSummary()
+    orderDisplay.innerHTML = ''
+})
 
 function toggleSummary() {
     document.querySelector('.summary').classList.toggle('hidden')
     document.querySelector('.customize').classList.toggle('hidden')
 }
 
-//Saving order functionality
-const saveOrderBtn = document.querySelector('#saveOrder')
-saveOrderBtn.addEventListener('click', () => {
+//Placing order functionality
+const placeOrderBtn = document.querySelector('#placeOrder')
+placeOrderBtn.addEventListener('click', () => {
     let customerName = document.querySelector('#customerName').value
+
 
     //Customer has to enter a name for their order
     if (!customerName) {
         reply.children[1].textContent = ''
         reply.firstChild.textContent = 'Please enter a name for your order!'
         reply.classList.toggle('hidden')
+        return setTimeout(() => reply.classList.toggle('hidden'), 2500)
     } else {
-        let order = {customerName: customerName.value,
-            pancake: pancake,
-            deliveryMethod: document.querySelector('input[name="delivery"]:checked').id,
-            cost: `${basePrice + toppingsPrice + deliveryCost}`
-            }
-
-    
-        console.log(order)
-        savedOrders.push(order)
+        allOrders.push(ticket)
 
         //Update 'reply' message on success
         reply.firstChild.textContent = `Your order was successful! 🥳`
@@ -117,14 +191,7 @@ saveOrderBtn.addEventListener('click', () => {
         reply.classList.toggle('hidden')
 
         //Removing current pancake/customer info
-        toppingsPrice = 0; deliveryCost = 0; basePrice = 5;
-        document.querySelector('#type').value = 5;
-        document.querySelectorAll('input[name="topping"], input[name="extra"]').forEach((topping) => {
-            topping.checked = false
-        })
-        document.querySelector('#eatIn').checked = true;
-        document.querySelector('#customerName').value = ''
-        pancake = {pancakeBase: 'Classic', toppings: [], extras: []}
+        resetPancake()
     }
     
     setTimeout(() => {
@@ -133,4 +200,22 @@ saveOrderBtn.addEventListener('click', () => {
         toggleSummary()
     }, 3000)
 })
+
+
+function resetPancake() {
+    toppingsPrice = 0;
+    basePrice = 5;
+    document.querySelector('#type').value = 5;
+    document.querySelectorAll('input[name="topping"], input[name="extra"]').forEach((topping) => {
+        topping.checked = false
+    })
+    pancake = {pancakeBase: 'Classic', toppings: [], extras: []}
+    updatePrice()
+}
+
+
+
+
+
+
 
