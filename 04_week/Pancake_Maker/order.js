f7199773a95e5f01fc9ad22f190773e5dc33f9d1
[Athina Kantis] <[athina.kantis@gmail.com]>
2024-10-02 19:34:33 +0300

    add active styling functionality

--- 04_week/Pancake_Maker/order.js
+++ 04_week/Pancake_Maker/order.js
@@ -1,20 +1,23 @@
 
-const specialsBtn = document.querySelector('#specials')
-const customBtn = document.querySelector('#custom')
-const specialsPage = document.querySelector('.specials')
-const customPage = document.querySelector('.customize')
-const summary = document.querySelector('.summary')
+const [specialsBtn, customBtn, viewCartBtn] = document.querySelectorAll('#specials, #custom, #viewCart')
+const [specialsPage, customPage, summary] = document.querySelectorAll('.specials, .customize, .summary')
 
+
+
+let active = specialsBtn
 let currentPage = specialsPage
-function toggleOrderPage(el) {
-    currentPage.classList.toggle('hidden')
-    el.classList.toggle('hidden')
+function toggleOrderPage(el, e) {
+    active.classList.toggle('active')
+    e.target.classList.toggle('active')
+    currentPage.classList.toggle('active')
+    el.classList.toggle('active')
 
+    active = e.target
     currentPage = el
 }
 
-specialsBtn.addEventListener('click', () => toggleOrderPage(specialsPage))
-customBtn.addEventListener('click', () => toggleOrderPage(customPage))
 
 
-
+specialsBtn.addEventListener('click', (e) => toggleOrderPage(specialsPage, e))
+customBtn.addEventListener('click', (e) => toggleOrderPage(customPage, e))
+viewCartBtn.addEventListener('click', (e) => toggleOrderPage(summary, e))
